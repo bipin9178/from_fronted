1ed88dc26e3ff8119b566ae9b3020e4eec4d2224
bipin9178 <bipinpkanzariya@gmail.com>
2025-12-13 18:02:37 +0530

    ema ek const ek fronted usrl banayo and pasi access thay

--- src/App.jsx
+++ src/App.jsx
@@ -20,22 +20,22 @@ export default function App() {
   useEffect(() => {
     dispatch(loadUserFromStorage());
   }, [dispatch]);
-
+const frontendUrl = 'https://from-fronted-k8aq-6mzdph7s7-bipins-projects-0c3e442c.vercel.app';
   return (
     <>
       <UserNavbar />
 
       <Routes>
-        <Route path="https://from-fronted-k8aq-6mzdph7s7-bipins-projects-0c3e442c.vercel.app" element={<Home />} />
-        <Route path="https://from-fronted-k8aq-6mzdph7s7-bipins-projects-0c3e442c.vercel.app/login" element={<LoginForm />} />
-        <Route path="https://from-fronted-k8aq-6mzdph7s7-bipins-projects-0c3e442c.vercel.app/register" element={<Register />} />
-        <Route path="/change-password"  element={ <ChangePassword /> }/>
-        <Route path="/profile/:id" element={<Profile />} />
-        <Route path="/forget-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/all-submissions" element={<Card />} />
-        <Route path="/submit" element={<SubmissionForm />} />
-        <Route path="/my-submissions" element={<SubmissionList />} />
+        <Route path="/" element={<Home />} />
+        <Route path={`${frontendUrl}/login`} element={<LoginForm />} />
+        <Route path={`${frontendUrl}/register`} element={<Register />} />
+        <Route path={`${frontendUrl}/change-password`} element={<ChangePassword />} />
+        <Route path={`${frontendUrl}/profile/:id`} element={<Profile />} />
+        <Route path={`${frontendUrl}/forget-password`} element={<ForgotPassword />} />
+        <Route path={`${frontendUrl}/reset-password/:token`} element={<ResetPassword />} />
+        <Route path={`${frontendUrl}/all-submissions`} element={<Card />} />
+        <Route path={`${frontendUrl}/submit`} element={<SubmissionForm />} />
+        <Route path={`${frontendUrl}/my-submissions`} element={<SubmissionList />} />
       </Routes>
     </>
   );
