4ebe9a8330d1ae1670d44ba2797e0c4d595cd437
bipin9178 <bipinpkanzariya@gmail.com>
2025-12-28 12:25:00 +0530

    this code chaning

--- src/middleware/api.jsx
+++ src/middleware/api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://from-backend-1-8h27.onrender.com/api",
+  baseURL: "https://from-backend-1-8h27.onrender.com/api/",
 });
 
 api.interceptors.request.use((config) => {
